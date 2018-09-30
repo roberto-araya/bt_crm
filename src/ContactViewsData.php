f3a492334e457238feb74704b75c99f62d2bef9c
Roberto Araya <roberto.araya@btester.cl>
2018-09-30 20:46:13 -0300

    by roberto_araya: Add handler class to views_data in redhen_contact

---
+++ src/ContactViewsData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\bt_crm_contact;
+
+use Drupal\views\EntityViewsData;
+use Drupal\views\EntityViewsDataInterface;
+
+/**
+ * Provides Views data for Connection entities.
+ */
+class ContactViewsData extends EntityViewsData implements EntityViewsDataInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+    return $data;
+  }
+}
